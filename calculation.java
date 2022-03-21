1338283e4afced8708081869c43bddaf2028dfb3
上山慎二 <shinji@shinji.local>
2022-03-21 18:08:08 +0900

    [Update] 投稿3/21

---
+++ calculation.java
@@ -0,0 +1,10 @@
+public class calculation {
+  public static void main(String[] args) {
+    int a;      // ①変数宣言の文
+    int b;
+    a = 20;     // ②計算の文（代入）
+    b = a + 5;  // ②計算の文（足し算して代入）
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
