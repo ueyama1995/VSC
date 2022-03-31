853f75140351e434ed4ef2d6f82f190cd69e9c2a
ueyama1995 <shinji160829033205@gmail.com>
2022-03-31 19:10:30 +0900

    [Update] 3/31

---
+++ 練習3.java
@@ -0,0 +1,16 @@
+public class 練習3 {
+  public static void main(String[] args) {
+    int seibetsu = 0;
+    int age = 26;
+    System.out.println("こんにちは。");
+    if(seibetsu == 0) {
+      System.out.println("私は男です。");
+    } else {
+      System.out.println("私は女です。");
+    }
+    if(seibetsu == 0) {
+      System.out.println(age + "歳です。");
+    }
+    System.out.println("よろしくおねがいします。");
+  }
+}
